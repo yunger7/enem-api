c7f3bd300cf717323732ad398d4fcab0aaf1f175
Luis Galete <luisgalete6264@gmail.com>
2024-07-02 22:06:56 -0300

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,48 @@
-import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { Separator } from '@/components/ui/separator';
+import { CtaButton } from '@/components/ui/cta-button';
+import { Logo } from '@/components/logo';
 
 export default function Home() {
     return (
-        <div className="p-8">
-            <h1 className="mb-2">Hello world!</h1>
-            <Button size="sm">Click me</Button>
+        <div className="relative flex h-screen w-full flex-col items-center justify-center bg-white bg-dot-black/[0.3] dark:bg-black dark:bg-dot-white/[0.3]">
+            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+            <div className="max-w-2xl text-center">
+                <div className="flex flex-col items-center justify-center gap-2">
+                    <Logo />
+                    <h1 className="relative z-20 bg-gradient-to-b from-neutral-500 to-neutral-800 bg-clip-text pb-2 pt-2 text-4xl font-bold text-transparent dark:from-neutral-200 dark:to-neutral-500 sm:text-7xl">
+                        API ENEM
+                    </h1>
+                    <p>
+                        Dados de 2100+ questões do ENEM, dos anos de 2009 a
+                        2023.
+                    </p>
+                </div>
+                <Separator className="my-4" />
+                <div className="flex flex-col items-center justify-center gap-6">
+                    <p>
+                        Este site e sua API associada não são afiliados,
+                        endossados ou patrocinados pelo Instituto Nacional de
+                        Estudos e Pesquisas Educacionais Anísio Teixeira (INEP)
+                        ou pelo Ministério da Educação (MEC). Este é um projeto,
+                        sem fins lucrativos, criado pela comunidade open-source
+                        brasileira, para facilitar o acesso às questões do ENEM.
+                    </p>
+                    <p>
+                        Todos os dados apresentados foram obtidos de fontes
+                        abertas e são de domínio público conforme o Art. 8º da
+                        Lei de Direitos Autorais (Lei nº 9.610/1998), que exclui
+                        de proteção "os textos de tratados ou convenções, leis,
+                        decretos, regulamentos, decisões judiciais e outros atos
+                        oficiais".
+                    </p>
+                    <Link href="/docs">
+                        <CtaButton className="flex items-center justify-center space-x-2 bg-white text-sm text-black dark:bg-black dark:text-white">
+                            Ler documentação
+                        </CtaButton>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ components/ui/cta-button.tsx
@@ -0,0 +1,97 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+
+import { motion } from 'framer-motion';
+import { cn } from '@/lib/utils';
+
+type Direction = 'TOP' | 'LEFT' | 'BOTTOM' | 'RIGHT';
+
+export function CtaButton({
+    children,
+    containerClassName,
+    className,
+    as: Tag = 'button',
+    duration = 1,
+    clockwise = true,
+    ...props
+}: React.PropsWithChildren<
+    {
+        as?: React.ElementType;
+        containerClassName?: string;
+        className?: string;
+        duration?: number;
+        clockwise?: boolean;
+    } & React.HTMLAttributes<HTMLElement>
+>) {
+    const [hovered, setHovered] = useState<boolean>(false);
+    const [direction, setDirection] = useState<Direction>('TOP');
+
+    const rotateDirection = (currentDirection: Direction): Direction => {
+        const directions: Direction[] = ['TOP', 'LEFT', 'BOTTOM', 'RIGHT'];
+        const currentIndex = directions.indexOf(currentDirection);
+        const nextIndex = clockwise
+            ? (currentIndex - 1 + directions.length) % directions.length
+            : (currentIndex + 1) % directions.length;
+        return directions[nextIndex];
+    };
+
+    const movingMap: Record<Direction, string> = {
+        TOP: 'radial-gradient(20.7% 50% at 50% 0%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)',
+        LEFT: 'radial-gradient(16.6% 43.1% at 0% 50%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)',
+        BOTTOM: 'radial-gradient(20.7% 50% at 50% 100%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)',
+        RIGHT: 'radial-gradient(16.2% 41.199999999999996% at 100% 50%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)',
+    };
+
+    const highlight =
+        'radial-gradient(75% 181.15942028985506% at 50% 50%, #3275F8 0%, rgba(255, 255, 255, 0) 100%)';
+
+    useEffect(() => {
+        if (!hovered) {
+            const interval = setInterval(() => {
+                setDirection(prevState => rotateDirection(prevState));
+            }, duration * 1000);
+            return () => clearInterval(interval);
+        }
+    }, [hovered]);
+    return (
+        <Tag
+            onMouseEnter={(event: React.MouseEvent<HTMLDivElement>) => {
+                setHovered(true);
+            }}
+            onMouseLeave={() => setHovered(false)}
+            className={cn(
+                'relative flex h-min w-fit flex-col flex-nowrap content-center items-center justify-center gap-10 overflow-visible rounded-full border bg-black/20 decoration-clone p-px transition duration-500 hover:bg-black/10 dark:bg-white/20',
+                containerClassName,
+            )}
+            {...props}
+        >
+            <div
+                className={cn(
+                    'z-10 w-auto rounded-[inherit] bg-black px-4 py-2 text-white',
+                    className,
+                )}
+            >
+                {children}
+            </div>
+            <motion.div
+                className={cn(
+                    'absolute inset-0 z-0 flex-none overflow-hidden rounded-[inherit]',
+                )}
+                style={{
+                    filter: 'blur(2px)',
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                }}
+                initial={{ background: movingMap[direction] }}
+                animate={{
+                    background: hovered
+                        ? [movingMap[direction], highlight]
+                        : movingMap[direction],
+                }}
+                transition={{ ease: 'linear', duration: duration ?? 1 }}
+            />
+            <div className="z-1 absolute inset-[2px] flex-none rounded-[100px] bg-black" />
+        </Tag>
+    );
+}
