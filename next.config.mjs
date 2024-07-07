ae8b1c259463381b0c563bd146c56e2f94660e96
Luis Galete <luisgalete6264@gmail.com>
2024-07-07 12:54:21 -0300

    chore: add CORS headers in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/v1/:path*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET, POST, PUT, DELETE, OPTIONS',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'Content-Type, Authorization',
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
