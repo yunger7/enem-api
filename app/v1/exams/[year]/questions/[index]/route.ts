ab2723691b2dbc9949fd800160bfbc13dc72dbf9
Luis Galete <luisgalete6264@gmail.com>
2024-07-07 22:14:11 -0300

    feat(api): add `/exams/{year}/questions/{index}` GET endpoint

---
+++ app/v1/exams/[year]/questions/[index]/route.ts
@@ -0,0 +1,56 @@
+import { EnemApiError, handleAndReturnErrorResponse } from '@/lib/api/errors';
+import { getExamDetails } from '@/lib/api/exams/get-exam-details';
+import { getQuestionDetails } from '@/lib/api/questions/get-question-details';
+import type { NextRequest } from 'next/server';
+
+type Params = {
+    year: string,
+    index: string,
+}
+
+export async function GET(
+    request: NextRequest,
+    { params }: { params: Params }
+) {
+    try {
+        const searchParams = request.nextUrl.searchParams;
+        let language = searchParams.get('language');
+
+        const exam = await getExamDetails(params.year);
+
+        if (!exam) {
+            throw new EnemApiError({
+                code: 'not_found',
+                message: `No exam found for year ${params.year}`,
+            });
+        }
+
+        if (language && !exam.languages.find(lang => lang.value === language)) {
+            throw new EnemApiError({
+                code: 'bad_request',
+                message: `Language ${language} not found in exam`,
+            });
+        }
+
+        if (!language && exam.languages.length > 0) {
+            language = exam.languages[0].value;
+        }
+
+        const questionDetails = await getQuestionDetails({
+            year: params.year,
+            index: params.index,
+            language,
+        });
+
+        if (!questionDetails) {
+            throw new EnemApiError({
+                code: 'not_found',
+                message: `Question ${params.index} not found in exam ${params.year}`,
+            });
+        }
+
+        return Response.json(questionDetails);
+    } catch (error) {
+        return handleAndReturnErrorResponse(error);
+    }
+}
