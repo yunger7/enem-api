af07af627ba3689f18ad8130308eff0525442d2a
Luis Galete <luisgalete6264@gmail.com>
2024-07-30 21:04:15 -0300

    refactor: set default theme to dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
             >
                 <ThemeProvider
                     attribute="class"
-                    defaultTheme="system"
+                    defaultTheme="dark"
                     enableSystem
                     disableTransitionOnChange
                 >
