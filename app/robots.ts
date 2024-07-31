14981222ba112e0df0fe172b9b56c8e2cbc0eccf
Luis Galete <luisgalete6264@gmail.com>
2024-07-30 21:22:22 -0300

    chore: add sitemap and robots file

---
+++ app/robots.ts
@@ -0,0 +1,17 @@
+import { MetadataRoute } from 'next';
+import { headers } from 'next/headers';
+
+export default function robots(): MetadataRoute.Robots {
+    const headersList = headers();
+    let domain = headersList.get('host') as string;
+
+    return {
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+        ],
+        sitemap: `https://${domain}/sitemap.xml`,
+    };
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from 'next';
+import { headers } from 'next/headers';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const headersList = headers();
+    let domain = headersList.get('host') as string;
+
+    return [
+        {
+            url: `https://${domain}/`,
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        }
+    ]
+}
