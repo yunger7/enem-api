8ca74c8cea8e3855816f58dcad856fb0670afc9e
Luís Galete <luisgalete6264@gmail.com>
2024-08-04 18:27:35 -0300

    feat: seo improvements (#1)
    * feat: add metadata tags to improve SEO
    
    * refactor: add `main` tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,43 @@ import { Inter as FontSans } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { cn } from '@/lib/utils';
 
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 
 const fontSans = FontSans({
     subsets: ['latin'],
     variable: '--font-sans',
 });
 
+export const viewport: Viewport = {
+    themeColor: 'black',
+    colorScheme: 'dark'
+}
+
+const TITLE = 'API ENEM';
+const DESCRIPTION = 'API para consulta de provas e questões do ENEM';
+
 export const metadata: Metadata = {
-    title: 'API ENEM',
-    description: 'API para consulta de provas e questões do ENEM',
+    title: TITLE,
+    applicationName: TITLE,
+    generator: 'Next.js',
+    description: DESCRIPTION,
+    creator: 'yunger7',
+    keywords: ['ENEM', 'API', 'Open Source', 'Provas', 'Questões'],
+    authors: [{ name: 'yunger7', url: 'https://github.com/yunger7' }],
+    openGraph: {
+        title: TITLE,
+        siteName: TITLE,
+        description: DESCRIPTION,
+        type: 'website',
+        url: 'https://enem.dev',
+        locale: 'pt_BR',
+        countryName: 'Brazil',
+    },
+    twitter: {
+        title: TITLE,
+        description: DESCRIPTION,
+        card: 'summary_large_image',
+    },
 };
 
 export default function RootLayout({
