d8f3f481208498c0e2bc46164f0136e254bfa0f9
Luis Galete <luisgalete6264@gmail.com>
2024-08-11 10:50:49 -0300

    fix: fix unresolved file path at Vercel

--- lib/api/exams/get-exam-details.ts
+++ lib/api/exams/get-exam-details.ts
@@ -1,16 +1,21 @@
+import path from 'node:path';
 import { existsSync } from 'node:fs';
 import { readFile } from 'node:fs/promises';
 import { ExamDetailSchema } from '@/lib/zod/schemas/exams';
 
 export async function getExamDetails(year: string | number) {
-    if (!existsSync(`${process.cwd()}/public/${year}/details.json`)) {
+    const filePath = path.join(
+        process.cwd(),
+        'public',
+        `${year}`,
+        'details.json',
+    );
+
+    if (!existsSync(filePath)) {
         return null;
     }
 
-    const examRaw = await readFile(
-        `${process.cwd()}/public/${year}/details.json`,
-        'utf-8',
-    );
+    const examRaw = await readFile(filePath, 'utf-8');
 
     const exam = JSON.parse(examRaw) as typeof ExamDetailSchema;
 
--- lib/api/exams/get-exams.ts
+++ lib/api/exams/get-exams.ts
@@ -1,12 +1,11 @@
+import path from 'node:path';
 import { readFile } from 'node:fs/promises';
 import { ExamSchema } from '@/lib/zod/schemas/exams';
 
 export async function getExams() {
-    const examsRaw = await readFile(
-        `${process.cwd()}/public/exams.json`,
-        'utf-8',
-    );
+    const filePath = path.join(process.cwd(), 'public/exams.json');
 
+    const examsRaw = await readFile(filePath, 'utf-8');
     const exams = JSON.parse(examsRaw) as Array<typeof ExamSchema>;
 
     return exams.map(exam => ExamSchema.parse(exam));
